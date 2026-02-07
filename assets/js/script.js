139614512a57072c4161a2744a8510a799f975a2
lienekl <lienekl@yahoo.com>
2026-02-07 15:30:09 +0000

    adding javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-let 
+let inputList = getElementById("inputlist".value.trim());
+let toDoList = document.getElementById("list");
+let addButton = document.getElementById("button");
+
+addButton.addEventListener("click, addToList");
+
+function addToList() {
+
+    let newLi = document.createElement("li");
+    newLi = document.appendChild(toDoList);
+
+    // if (inputList)
+    // alert("Please write down the task");
+    // return;
+}
+
