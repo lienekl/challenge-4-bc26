d181fd39e497d5b2b156853cbf1eb04598a85d3b
lienekl <lienekl@yahoo.com>
2026-02-09 14:16:55 +0000

    final code teste + readme.md added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,14 @@ let addButton = document.getElementById("button");
 
 addButton.addEventListener("click", addToList);
 
-//checked class when a li is clicked
-ulEl.addEventListener("click", function (ev) {
-    if (ev.target.tagName === "li") {
-        ev.target.classList.toggle("checked");
+
+inputList.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        addToList();
     }
 });
+
+
 
 function addToList() {
 
@@ -36,19 +38,7 @@ function addToList() {
     }
 
     let listItems = document.createElement("li");
-    // Create checkbox
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.className = "task-checkbox";
 
-    // When checkbox is clicked, toggle completed style
-    checkbox.addEventListener("change", function () {
-        if (this.checked) {
-            taskSpan.classList.add("checked");
-        } else {
-            taskSpan.classList.remove("checked");
-        }
-    });
 
     // Create task text span
     let taskSpan = document.createElement("span");
@@ -66,7 +56,8 @@ function addToList() {
             document.getElementById("list-container").style.display = "none";
         }
     }
-    listItems.appendChild(checkbox);
+   
+    
     listItems.appendChild(taskSpan);
     listItems.appendChild(closeBtn);
     ulEl.appendChild(listItems);
@@ -79,71 +70,3 @@ function addToList() {
 
 
 }
-
-
-// let closeEl = document.getElementsByClassName("close");
-// for (i = 0; i < closeEl.length; i++) {
-//     closeEl[i].onclick = function () {
-//         var div = this.parentElement;
-//         div.style.display = "none";
-//     }
-// }
-
-
-// let span = document.createElement("span");
-// let txt = document.createTextNode("x");
-// span.className = "close";
-// span.appendChild(txt);
-// listItems[i].appendChild(span);
-
-// ulEl.addEventListener("click", function (ev) {
-//     if (ev.target.tagName === newLi) {
-//         ev.target.classList.toggle("checked");
-//     }
-// }, false);
-
-// // Create a "close" button and append it to each list item
-// let li = document.getElementsByTagName("li");
-
-// for (i = 0; i < li.length; i++) {
-//   var span = document.createElement("span");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   li[i].appendChild(span);
-// }
-
-// let close = document.getElementsByClassName("close");
-
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function() {
-//     let div = this.parentElement;
-//     div.style.display = "none";
-//   }
-// }
-
-// let checkbox = li.querySelector("input");
-// let editBtn = li.querySelector(".edit-btn");
-// let taskSpan = li.querySelector("span");
-// let deleteBtn = li.querySelector(".delete-btn");
-
-
-// checkbox.addEventListener("click", function () {
-//     li.classList.toggle("completed", checkbox.checked);
-
-// });
-
-// editBtn.addEventListener("click", function () {
-//     let update = prompt("Edit task:", taskSpan.textContent);
-//     if (update !== null) {
-//         taskSpan.textContent = update;
-//         li.classList.remove("completed");
-//         checkbox.checked = false;
-//     }
-// });
-
-// deleteBtn.addEventListener("click", function () {
-//     if (confirm("Are you sure you want to delete this task?")) {
-//         li.remove();
-//     }
-// });
---
+++ readme.md
@@ -0,0 +1,21 @@
+# The Interactive TODO List
+
+This project was created as part of a Week 4 challenge to demonstrate JavaScript knowledge and skills.
+
+The goal of the challenge was to build an interactive TODO list application that allows users to manage tasks dynamically using JavaScript.
+
+## Key features
+
+'*' Users can add new TODO tasks to the list
+
+'*' New tasks are displayed dynamically on the page without reloading
+
+'*' Input validation is implemented:
+
+    '*' If the input field is empty, an alert notifies the user:
+        “Please write down your task!”
+
+    '*' If the task already exists in the list, a prompt notifies the user:
+        “This task is already in your list!”
+
+'*' A Delete function allows users to remove tasks once they are completed
